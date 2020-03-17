39156cb8fc0d45d24c35672607cd3bcf9598b824
Sahil-Bansal-prog <sahilbansal1233@gmail.com>
2020-03-17 18:26:57 +0530

    Hello, A little bit Fun is there in my file

---
+++ CdingWar1.py
@@ -0,0 +1,99 @@
+# # 1.
+# # vowels=['a','e','i','o','u']
+# # word=input(" Enter a Number to search for vowels: ")
+# # fount=[]
+# # for letter in word:
+# #     if letter in vowels:
+# #         if letter not in fount:
+# #             fount.append(letter)
+# #
+# # for vowel in fount:
+# #         print(vowel)
+# # 2.
+# phrase="Don't Panic!"
+# plist=list(phrase)
+# print(phrase)
+# print(plist)
+#
+# new_phrase=''.join(plist[1:3])
+# new_phrase=new_phrase+''.join([plist[5],plist[4],plist[-5],plist[-6]])
+#
+# print(plist)
+# print(new_phrase)
+#
+
+p="marvin"
+l=list(p)
+for char in l:
+    print(char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t',char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*2,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*3,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*4,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*5,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*6,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*7,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*8,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*9,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*10,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*11,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*12,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*13,char)
+
+p="marvin"
+l=list(p)
+for char in l:
+    print('\t'*14,char)
+
